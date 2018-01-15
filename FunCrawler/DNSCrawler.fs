5ec248759aa258c7a11c33512d94f7123ea5a633
Rinorragi <rinorragi@gmail.com>
2018-01-15 23:18:13 +0200

    Introduce simple DNS information gathering

---
+++ FunCrawler/DNSCrawler.fs
@@ -0,0 +1,20 @@
+﻿namespace FunCrawler
+module DNSCrawler =
+    open System
+    open DnsClient
+
+    type DNSCrawlerResult(initDomain:string, initDnsResponse:IDnsQueryResponse) = 
+        member this.DnsResponse:IDnsQueryResponse = initDnsResponse
+        member this.Domain:string = initDomain
+
+    let getDNSCrawlerClient() =
+        let client = new LookupClient()
+        client.UseCache <- false
+        client
+
+    let getDNSCrawlerResultAsync(client:LookupClient, domain:string) = 
+        async {
+            let! result = client.QueryAsync(domain, QueryType.ANY) |> Async.AwaitTask
+            let crawlResponse = DNSCrawlerResult (domain, result)
+            return crawlResponse
+        }
--- FunCrawler/Program.fs
+++ FunCrawler/Program.fs
@@ -6,24 +6,29 @@ module Program =
     open System.Net.Http.Headers
     open System.Net
     open HttpCrawler
+    open DNSCrawler
 
-    let printResponse(entry:HttpCrawlerResult) =
+    let printHttpResponse(entry:HttpCrawlerResult) =
         printfn "Result for url %s was: %s v%s" entry.Url (entry.StatusCode.ToString()) entry.Version
-
         // This is collection<string,collection<string>>
         entry.Headers
         |> Seq.iter (fun h -> printfn "Header: %s Values: [%s]" h.Key (h.Value |> Seq.fold (fun v v2 -> v + "(" + v2 + ")") ""))
 
+    let printDNSResponse(entry:DNSCrawlerResult) =
+        printfn "Result for domain %s is:" entry.Domain
+        // This is
+        entry.DnsResponse.AllRecords
+        |> Seq.iter (fun d -> printfn "%s" (d.ToString()))
+
     [<EntryPoint>]
     let main argv =
-        let httpClient = getHttpCrawlerClient()
         // We want to have few maps that have a 
         let domains = [
             "dev.solita.fi";
-            "www.solita.fi"; 
-            "www.wikipedia.com"; 
-            "www.stackoverflow.com"]
+            "www.solita.fi"]
     
+        // HTTP
+        let httpClient = getHttpCrawlerClient()
         let httpResults = 
             domains 
             |> List.map (fun url -> getHttpCrawlerResultAsync (httpClient, url, "http://"))
@@ -33,8 +38,7 @@ module Program =
             |> Map.ofArray
         printfn "HTTP results!"
         httpResults
-        |> Map.iter (fun k v -> printResponse(v))
-    
+        |> Map.iter (fun k v -> printHttpResponse(v))    
         let httpsResults = 
             domains 
             |> List.map (fun url -> getHttpCrawlerResultAsync (httpClient, url, "https://"))
@@ -44,8 +48,22 @@ module Program =
             |> Map.ofArray 
         printfn "HTTPS results!"
         httpsResults 
-        |> Map.iter (fun k v -> printResponse(v))
+        |> Map.iter (fun k v -> printHttpResponse(v))
+
+        // DNS
+        let dnsClient = getDNSCrawlerClient()
+        let dnsResults = 
+            domains
+            |> List.map (fun domain -> getDNSCrawlerResultAsync (dnsClient, domain))
+            |> Async.Parallel
+            |> Async.RunSynchronously
+            |> Array.map(fun result -> (result.Domain, result))
+            |> Map.ofArray
+        printfn "DNS results!"
+        dnsResults
+        |> Map.iter (fun k v -> printDNSResponse(v))
 
         // Just to make it pause
+        printfn "Finished! Press any key..."
         let _ = Console.ReadLine()
         0 // return an integer exit code
